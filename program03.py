5b086aa8b77fa2f446d7e03cecbb3be96b7ad1b8
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-10 22:00:19 +0300

    misol

--- program03.py
+++ program03.py
@@ -1,1 +1,11 @@
-   
+answer = "Toshkent"
+
+trise = 0
+while True:
+    option = input("Ozbekiston poytaxt ")
+    trise += 1
+    if option.capitalize() == answer:
+        print(f"szi togri topdiz,urinishlar soni: {trise}")
+        break
+
+
