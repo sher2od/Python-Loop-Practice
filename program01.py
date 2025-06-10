f0adf22deb2955c25fa81068958613fc206e81c3
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-10 21:51:24 +0300

    misol

---
+++ program01.py
@@ -0,0 +1,144 @@
+text = "Salom"
+i = 0
+while i < 7:
+    print(text)
+    i += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from time import sleep
+
+# for i in range(30):
+#     print(i)
+#     sleep(1)
+
+''''''
+# text = "sherzodbek"
+
+# for i, v in enumerate(text, start=1):
+#     print(i,v) 
+
+''''''
+# import time
+
+# start_time = time.time()
+
+# for i in range(100000000000):
+#     pass
+
+# end_time = time.time()
+
+# print(end_time - start_time)
+''''''
+# i = 0
+
+# while i < 10 + 1:
+#     print(i)
+#     i +=1
+
+''''''
+
+# from random import randint
+
+# random_number = randint(0,100)
+
+# for i in range(100):
+#     if i == random_number:
+#         print(f"son topildi {1}")
+#         break
+
+''''''
+
+# from random import randint
+
+# random_number = int(input("son >> "))
+
+
+# for i in range(100):
+#     if i == random_number:
+#         print(f"son topildi: {i} >> {random_number}")
+#         break
+
+# else:
+#     print("son yoe")
+
+''''''
+
+# for i in range(10):
+#     print(f"num:{i}")
+
+#     if i == 1:
+#         pass
+#     if i == 5:
+#         continue
+#     if i == 8:
+#         break
+#     print(f"kv num:{i*i}")
+
+''''''
+
+# for i in range(1,5):
+#     print(f"{i}^2={pow(i,2)}")
+
+''''''
+
+# answer = "Toshkent"
+
+# for i in range(3):
+#     opation = input("Ozbekiston pytaxt ")
+#     if opation.capitalize() == answer:
+#         print("good")
+#         break
+# else:
+#     print("nooo")
+
+''''''
+
+# answer = "Toshkent"
+
+# trise = 0
+# while trise < 3:
+#     option = input("Ozbekiston poytaxt ")
+#     trise += 1
+#     if option.capitalize() == answer:
+#         print(f"szi togri topdiz,urinishlar soni: {trise}")
+#         break
+
+# else:
+#     print(f"siz topolmadiz urunishlar soni {trise}")
+
+''''''
+
+# n = input("son ")
+
+# s = 0
+# for i in n:
+#     s += int(i)
+
+# print(s)
+
+''''''
+
+# n = int(input("son >> "))# 12345
+
+# s = 0
+# while n != 0:
+#     s += n % 10   # s += 5
+#     n //= 10 # s = 1234
+
+# print(s)
---
+++ program02.py
@@ -0,0 +1,10 @@
+for i in range(1,5):
+     print(f"{i}^2={pow(i,2)}")
+
+
+''''''
+s = 1
+while s < 6:
+    print(f"{s}^2 = {pow(s,2)}")
+    s += 1
+
