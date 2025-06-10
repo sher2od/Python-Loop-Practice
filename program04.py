204d992747a44d14fa5ce59827ea1326243a61b4
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-10 22:20:26 +0300

    misol04

---
+++ program04.py
@@ -0,0 +1,11 @@
+from random import randint
+
+
+while True:
+    pasword = int(input("parol >> "))
+    if pasword == randint(1,1000):
+        print("parol togri")
+        break
+
+    else:
+        print("yana urining")
